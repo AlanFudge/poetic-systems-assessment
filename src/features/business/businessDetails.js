30009cc81b674086bbc57e90bf9cbd11161260b8
unknown <af320040@gmail.com>
2021-12-08 22:30:00 -0600

    Create component for handling business details. Placeholder is used until designed is finished

---
+++ src/features/business/businessDetails.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { useParams } from "react-router-dom";
+import { getBusiness } from "./businessSlice";
+
+export default function BusinessDetails() {
+    const dispatch = useDispatch();
+    const { businessId } = useParams();
+    useEffect(() => {
+        dispatch(getBusiness(businessId));
+    });
+
+    return (
+        <>
+            <h1>Business Details Page</h1>
+            <p>This page fetched the business by id in URL, adds it to state, and will display that information via a display component</p>
+        </>
+    )
+}
