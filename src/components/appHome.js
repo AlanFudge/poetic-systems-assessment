228a4f138c383a4bcd3c5294b9dbebc1bcd25ac7
unknown <af320040@gmail.com>
2021-12-09 01:44:14 -0600

    create placeholder landing page component and search bar

---
+++ src/components/appHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "./searchBar";
+
+export default function AppHome() {
+    return (
+        <>
+            <h1>Landing page</h1>
+            <p>this is the landing page and contains the SearchBar component</p>
+            <SearchBar />
+        </>
+    )
+}
---
+++ src/components/searchBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SearchBar({ location, term, sortBy }) {
+    return (
+        <p>This is the search bar component</p>
+    )
+}
