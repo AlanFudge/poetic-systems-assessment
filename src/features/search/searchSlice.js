00d8fd70ebf37c45abb1de59361123e6472b9e6e
Alan Fudge <af320040@gmail.com>
2021-12-10 19:03:09 -0600

    fix typo in sortBy query param that caused the wrong string encode; use encodeURIComponent on user input to properly encode query parameters

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -3,9 +3,9 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const getSearchResults = createAsyncThunk('search/getSearchResults', async ({ location, term, sortBy }) => {
     if (location === null) location = '76208'
     if (sortBy === null) sortBy = 'distance';
-    let queryURL = `https://assorted-cultured-hearing.glitch.me/search?sort_by=${sortBy}`;
-    if (location) queryURL += `&location=${location}`;
-    if (term) queryURL += `&term=${term}`;
+    let queryURL = `https://assorted-cultured-hearing.glitch.me/search?sortBy=${sortBy}`;
+    if (location) queryURL += `&location=${encodeURIComponent(location)}`;
+    if (term) queryURL += `&term=${encodeURIComponent(term)}`;
 
     return fetch(queryURL).then(res => res.json());
 }
